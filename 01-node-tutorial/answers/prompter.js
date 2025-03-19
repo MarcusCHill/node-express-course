2062968b21ff5ef86a7cfe2f8dddf8a8c910a61a
Marcus Hill <hillmarcus32@gmail.com>
2025-03-18 22:46:55 -0400

    12-http.js and promper.js

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -21,17 +21,22 @@ const getBody = (req, callback) => {
 };
 
 // here, you could declare one or more variables to store what comes back from the form.
-let item = "Enter something below.";
+let pokeType = "Did you know the pokemon Arceus can change types? Choose your favorite pokemon type!";
+let types = ["Grass", "Fire", "Water", "Electric", "Psychic", "Figiting", "Dark", "Metal", "Normal"]
 
 // here, you can change the form below to modify the input fields and what is displayed.
 // This is just ordinary html with string interpolation.
 const form = () => {
   return `
-  <body>
-  <p>${item}</p>
-  <form method="POST">
-  <input name="item"></input>
-  <button type="submit">Submit</button>
+  <body style="background-color:lightgrey">
+  <form method="POST" style="height: 100vh; width: 100vw; display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
+  <label for="pokeType" style="color:black; font-size: 2rem;">${pokeType}</label>
+  </br>
+  <select id="pokeType" name="pokeType" style="width: 25vw; padding: 2rem;">
+    ${types.map(type => `<option value=${type}>${type}</option>`).join('')}
+  </select>
+  </br>
+  <button type="submit" style="width: 10vw; padding: 1rem;">Submit</button>
   </form>
   </body>
   `;
@@ -44,16 +49,40 @@ const server = http.createServer((req, res) => {
     getBody(req, (body) => {
       console.log("The body of the post is ", body);
       // here, you can add your own logic
-      if (body["item"]) {
-        item = body["item"];
+      if (body["pokeType"] !== "Normal") {
+        res.writeHead(303, {'Content-Type': 'text/html'})
+        res.end(`
+          <body style="background-color:lightgrey">
+            <div style="height: 100vh; width: 100vw; display: flex; flex-direction: column; justify-content: space-around; align-items: center;"> 
+            <h1 style="font-size: 4rem;">${body["pokeType"]} Arceus</h1>
+            <p style="font-size: 2rem;">${body["pokeType"]} Arceus occurs when you let Arceus hold a ${body["pokeType"]} type plate!</p>
+            <a href="/" style="font-size: 1.5rem;">Return to form</a>
+            </div>
+          </body>
+        `)
+      } else if (body["pokeType"] === "Normal"){
+        res.writeHead(303, {'Content-Type': 'text/html'})
+        res.end(`
+          <body style="background-color:lightgrey">
+            <div style="height: 100vh; width: 100vw; display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
+            <h1 style="font-size: 4rem;">${body["pokeType"]} Arceus</h1>
+            <p style="font-size: 2rem;">${body["pokeType"]} Arceus occurs natrually, without the need of a plate to decide its type!</p>
+            <a href="/" style="font-size: 1.5rem;">Return to form</a>
+            </div>
+          </body>
+        `)
       } else {
-        item = "Nothing was entered.";
+        res.writeHead(400, {'Content-Type': 'text/html'})
+        res.end(`
+          <body style="background-color:lightgrey">
+            <div style="height: 100vh; width: 100vw; display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
+            <h1 style="font-size: 4rem;">Oops!</h1>
+            <p style="font-size: 2rem;">Something went wrong here!??!</p>
+            <a href="/" style="font-size: 1.5rem;">Return to form</a>
+            </div>
+          </body>
+        `)
       }
-      // Your code changes would end here
-      res.writeHead(303, {
-        Location: "/",
-      });
-      res.end();
     });
   } else {
     res.end(form());
