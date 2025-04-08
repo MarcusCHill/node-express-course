172672af553c77ec4406524074a11a888172ffa0
Marcus Hill <hillmarcus32@gmail.com>
2025-04-08 19:38:17 -0400

    Update from week1 PR

--- 01-node-tutorial/answers/12-http.js
+++ 01-node-tutorial/answers/12-http.js
@@ -1,15 +1,18 @@
 const http = require('http')
 
+//Cannot write after end. In my previous case I called res.end twice once through conditional and then after conditional is completed.
+
 const server = http.createServer((req, res) => {
     if(req.url === '/'){
         res.end('Welcome to our home pageeeee')
-    }
-    if(req.url === '/about'){
+        //return
+    } else if(req.url === '/about'){
         res.end("Blah blah blah about about about")
-    }
-    res.end(`<h1>Oops! :0</h1>
+    } else {
+        res.end(`<h1>Oops! :0</h1>
         <p>We can't find what you are looking for :(</p>
         <a href="/">Go Home!</a>`)
+    }
 })
 
 server.listen(7290)
